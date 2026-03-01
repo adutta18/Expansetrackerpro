7dfd3dcb198bbd94c4ee36cb81a4e0e2bcd9a17b
adutta18 <ajoydutta134@gmail.com>
2026-03-01 22:23:22 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,12 @@
-const CACHE_NAME = "expense-cache-v1";
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/script.js",
-  "/manifest.json"
-];
-
-// Install
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-// Fetch
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-// Activate (cleanup old caches)
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("tracker-cache").then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/style.css",
+        "/script.js"
+      ]);
     })
   );
 });
